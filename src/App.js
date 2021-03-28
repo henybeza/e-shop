18754655c1abf53639d7e4342ce88d89758f31bf
henybeza <henny8000@gmail.com>
2021-03-28 08:48:47 -0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
+import React, {  useState } from 'react';
+import { Button } from '@material-ui/core';
+ 
+import Header from './Components/Header';
+import Items from './Components/Items';
 import './App.css';
-
 function App() {
+  const [counter, setCounter] = useState(0);
+  const increamentCounter = () => {setCounter(counter+1);};
+  const itemStyle={display:"flex", backgroundColor:"red", margin:"10px",justifyContent:"center"};
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <div style={itemStyle}>
+      <Items />
+      <Items />
+      <Items />
+      <Items />
+      </div>
+      
+      <MyButton handleClick= {increamentCounter} name="Add to Cart" />
+      <Display myCounter={counter}/>
+      <YourButton name="ORDER NOW"/>
     </div>
   );
 }
+
+function MyButton(props){
+  const {handleClick, name}= props;
+  return (
+    <div>   
+    <Button onClick={handleClick} variant="contained" color="secondary">
+      {name}
+    </Button>   
+    </div>
+  );
+}
+
+function Display(props){
+  const {myCounter} = props;
+return (
+  <div><h1>{myCounter}</h1></div>
+);
+}
+
+ function YourButton(props){
+   const [counter, setCounter] = useState(0);
+   const increamentCounter = ()=>{setCounter(counter+1);}
+   return(
+     <div>
+     <h1>{props.name}</h1>
+      <Button onClick={increamentCounter} variant="contained" color="primary">
+        
+        {counter}
+      </Button>
+     </div>
+   );
+ }
 
 export default App;
---
+++ src/Components/theme.js
@@ -0,0 +1,17 @@
+import { createMuiTheme, useTheme } from '@material-ui/core/styles';
+import { grey, deepPurple, amber } from '@material-ui/core/colors';
+const theme = createMuiTheme({
+
+  palette: {
+
+    primary: {
+
+      main: deepPurple[500],
+
+    },
+
+
+  },
+
+});
+export default theme;
